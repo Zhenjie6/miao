c9dfaa696320878cc99a6125969aa7436c70d27e
ZhenJie <cn.zhenjie@gmail.com>
2020-04-29 12:12:38 +0800

    lhlkjasldfj

--- lodash/jackyen96-lodash.js
+++ lodash/jackyen96-lodash.js
@@ -1,19 +1,77 @@
 var jackyen96 = {
-  compact: function(ary){
+  compact(ary) {
     return ary.filter(item => item);
   },
 
-  fill: function(array, value, start = 0, end = array.length){
+  fill(array, value, start = 0, end = array.length) {
+    let result = new Array(array)
     if(start >= end)
-    return array
-    for(let i = start; i < end; i++){
+      return result
+    for(let i = start; i < end; i++) {
       array[i] = value
     }
+    return result
   },
 
-  isNull(val){
+  isNull(val) {
     return val === null ? true : false
   },
 
+  /**
+   * @description Creates an array of elements split into groups the length of size.
+   * If array can't be split evenly, the final chunk will be the remaining elements.
+   * 
+   * @param ary{Array} 需要拆分的数组
+   * @param size{Number} 拆分的单位大小
+   * @returns {Array} 拆分后组成的新数组
+   */
+  chunk(ary, size) {
+    let result = new Array()
+    for(let i = 0; i <= ary.length; i += size) {
+      result.push(ary.slice(i, i + size))
+    }
+    return result
+  },
+
+  /**
+   * @description 把数组和新的内容链接起来
+   * @param array {Array}: The array to concatenate.
+   * @param values {...*}: The values to concatenate.
+   */
+  concat(array, values) {
+    let result = array.slice(0)
+    for(let arg in arguments) {
+      if(typeof arg === Array) {
+        let ary = new Array()
+        for(let i = 0; i < arg.length; i++) {
+          ary.push(arg[i])
+        }
+        result.push(ary)
+        continue
+      }
+      result.push(arg)
+    }
+    return result
+  },
+  difference(array, value = []) {
+    let result = new Array()
+    let map = {}
+    for(let item in value){
+      if(map[item])
+      continue
+      map[item] = true
+    }
+    for(let i = 0; i < array.length; i++){
+      if(map[array[i]])
+      continue
+      result.push(array[i])
+    }
+    return result
+  },
+  drop,
+  fill,
+  find,
+  first,
+  head,
 
 }
